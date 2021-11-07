d6b901cea1e3b803e1987618c8a90f6c4062c85d
Bel Lloyd <bel@zandiconsulting.com>
2021-11-07 21:04:44 +1000

    updated logo link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
 
     return (
         <div class="header">
-            <a href="http://localhost:3000/">
+            <a href="https://fundare.herokuapp.com/">
                 <img src="https://raw.githubusercontent.com/bel-lloyd/my-files/master/Fundare%20Logo%20(1).svg"/>
             </a>
             <h3>Do a dare or fund a dare to show you care.</h3>
